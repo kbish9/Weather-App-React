ac8b6492b70abde05d1f3bd92d55a7589e08af13
kbish9 <103216809+kbish9@users.noreply.github.com>
2022-04-28 20:32:56 +0100

    Finished App Set Up

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,7 +3,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-     Hello Weather
+      Hello Weather
+      <div>
+        Created by Kelsey Bishop and is 
+        <a href="https://github.com/kbish9/Weather-App-React" target="_blank" rel="noreferrer">
+          Open Source
+        </a>
+      </div>
     </div>
   );
 }
