f7a901d2a3b51cd5975e1c382d14887e660d210b
kbish9 <103216809+kbish9@users.noreply.github.com>
2022-04-29 19:52:20 +0100

    added axios and api

--- my-app/src/Weather.js
+++ my-app/src/Weather.js
@@ -1,39 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
 export default function Weather() {
-  return (
-    <div className="Weather">
-      <form>
-        <div className="row search">
-          <div className="col-9">
-              <input type="search" placeholder="Enter a city..." className="form-control" autoFocus="on"/>
+const [ready, setReady] = useState(false);
+const [weatherData, setWeatherData] = useState(ready, false);
+function getWeather(response) {
+    setWeatherData({
+        ready: true,
+        Date: "Date",
+        Temp: response.data.main.temp,
+        Wind: response.data.wind.speed,
+        City: response.data.name,
+        Icon: "to follow",
+        Description: response.data.weather[0].description,
+    });
+    setReady(true);
+}
+
+if(weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row search">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city..."
+                className="form-control"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-3">
+              <input type="submit" value="Search" className="btn" />
+            </div>
           </div>
-          <div className="col-3">
-              <input type="submit" value="Search" className="btn"/>
+        </form>
+        <div className="container-weather">
+          <div className="info">
+            <h1>{weatherData.City}</h1>
+            <h4>{weatherData.Date}</h4>
+            <h4>{weatherData.Icon}</h4>
+          </div>
+          <div className="info">
+            <h2>{Math.round(weatherData.Temp)} °C|F</h2>
+            <h6>Current Temp</h6>
+            <h4 className="text-capitalize">{weatherData.Description}</h4>
+            <h4>{Math.round(weatherData.Wind)}</h4>
           </div>
         </div>
-      </form>
-      <div className="container-weather">
-        
-           
-          <div className="info">
-            <h1>London</h1>
-            <h4>Thursday 8pm</h4>
-            <h4>
-                Icon
-                </h4>    
-          </div>
-          <div className="info">
-            <h2>8 C|F</h2>
-            <h6>
-                Current Temp
-            </h6>
-            <h4>Sunny</h4>
-            <h4>Wind: 8km/h</h4>
-          </div>
-        
       </div>
-    </div>
-  );
+    );
+} else {
+     const apiKey = "d8e366c15b60dabbe9f54b799921805a";
+    let city = "London"
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(getWeather);
+}
+
+   
+
+
 }
