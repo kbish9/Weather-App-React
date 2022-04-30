5e9e324f7c461edf5ef3f1d7222be69fd0ea571f
kbish9 <103216809+kbish9@users.noreply.github.com>
2022-04-30 22:04:35 +0100

    update week forecast with useEffect

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./Weather.css";
 import ForecastDay from "./ForecastDay";
@@ -6,38 +6,34 @@ import ForecastDay from "./ForecastDay";
 export default function Forecast(props){
    let [ready, setReady] = useState(false);
    let [forecast, setForecast] = useState(null);
+   useEffect(() => {
+    setReady(false);
+   }, [props]);
+
    function handleResponse(response) {
     setForecast(response.data.daily);
     setReady(true);
-
    }
    
     if (ready) {
         return(
     <div className="row forecast">
-        <div className="col">
-          <ForecastDay data={forecast[1]}/>
-        </div>
-        <div className="col">
-          <ForecastDay data={forecast[2]}/>
-        </div>
-        <div className="col">
-          <ForecastDay data={forecast[3]}/>
-        </div>
-        <div className="col">
-          <ForecastDay data={forecast[4]}/>
-        </div>
-        <div className="col">
-          <ForecastDay data={forecast[5]}/>
-        </div>
+        {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+                return (
+                <div className="col" key={index}>
+                    <ForecastDay data={dailyForecast}/>
+                </div>  
+                );
+            }
+        })}   
     </div>
         );
     } else {
-
     let apiKey ="d8e366c15b60dabbe9f54b799921805a";
     let lat = props.coord.lat;
     let lon = props.coord.lon;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=-${lon}&exclude=hourly,daily&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=-${lon}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
     return null;
 
